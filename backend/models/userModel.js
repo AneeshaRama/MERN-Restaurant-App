40437211101570f11634af9b61b5a215dfbd6407
AneeshaRama <aneesharama@gmail.com>
2022-05-22 20:48:14 +0530

    Created user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("express");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    address: {
+      type: String,
+    },
+  },
+  { timeStamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
