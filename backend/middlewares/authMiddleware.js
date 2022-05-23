7460c858a6e5eeb2e9518234f42874f6eba10dec
AneeshaRama <aneesharama@gmail.com>
2022-05-23 08:48:50 +0530

    Added admin check middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const Users = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(400).json({ message: "Please login to continue" });
+  const tokenString = authHeader.split(" ");
+  const token = tokenString[1];
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (!decoded)
+    return res.status(400).json({ message: "Invalid Authentication." });
+
+  const user = await Users.findOne({ _id: decoded._id });
+  if (user.role !== "admin") {
+    return res.status(400).json({ message: "You are not authorized" });
+  }
+  next();
+};
+
+module.exports = authMiddleware;
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const foodController = require("../controllers/foodController");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/food/new", foodController.addFood);
+router.post("/food/new", authMiddleware, foodController.addFood);
 router.get("/foods", foodController.getAllFoods);
 router.get("/food/:id", foodController.getFoodDetails);
-router.put("/food/:id", foodController.updateFood);
-router.delete("/food/:id", foodController.deleteFood);
+router.put("/food/:id", authMiddleware, foodController.updateFood);
+router.delete("/food/:id", authMiddleware, foodController.deleteFood);
 
 module.exports = router;
